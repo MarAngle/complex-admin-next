c26308e810e0d5c450a1cc5d692cc901a23df43d
shenguoqing <yinweiyingzhe@126.com>
2022-08-23 11:43:26 +0800

    升级适配

--- src/main/complex/data.ts
+++
@@ -1,5 +0,0 @@
-import antdImplement from "@/modules/complex-data/implement/antd"
-
-export const dataInit = function() {
-  antdImplement.init()
-}
--- src/main/complex/index.ts
+++ src/main/complex/index.ts
@@ -1,12 +1,10 @@
 import { App } from 'vue'
 import { componentInit } from './component'
-import { dataInit } from './data'
 import { funcInit } from './func'
 import { requireInit } from './require'
 
 export const init = function(app: App) {
   requireInit()
-  dataInit()
   componentInit(app)
   return funcInit(app)
 }
